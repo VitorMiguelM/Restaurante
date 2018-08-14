a4a371831071ee17bfb80213f8f5d530ebed37e7
Laura bueno <104965@proway.treina>
2018-08-14 17:31:03 -0300

    classes restaurante
    nao acabada
    em andamento

---
+++ TrabalhoFinal-Restaurante/TrabalhoFinal-Restaurante/Models/clientes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrabalhoFinal_Restaurante.Models
+{
+    public class clientes
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nome não pode ser vazio")]
+        [MinLength(3,ErrorMessage="Nome deve estar completo no minimo 3 carateres")]
+        [MaxLength(100, ErrorMessage = "Nome deve conter no máximo 100 caracteres")]
+        public string NomeCompleto { get; set; }
+
+        public string  Login { get; set; }
+        public string Senha { get; set; }
+        public double Celular { get; set; }
+        public string DataNacimento{ get; set; }
+        public double CPF { get; set; }
+        public string Estado { get; set; }
+        public string Cidade  { get; set; }
+        public string Bairro { get; set; }
+        public string Logadouro { get; set; }
+        public string cep { get; set; }
+    }
+}
---
+++ TrabalhoFinal-Restaurante/TrabalhoFinal-Restaurante/Models/funcionarios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrabalhoFinal_Restaurante.Models
+{
+    public class funcionarios
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Senha { get; set; }
+        public  double Celular { get; set; }
+        public double DataDeNacimento { get; set; }
+        public double cpf { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public string Logadouro { get; set; }
+        public double cep { get; set; }
+        public string Cargo { get; set; }
+
+
+
+    }
+}
