5909fb2616aa9afcddf2a5783c9b84d580d2889b
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2018-08-16 15:03:04 -0300

    ?/

---
+++ TrabalhoFinal-Restaurante/TrabalhoFinal-Restaurante/DataBase/Funcionarios.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE funcionarios(
+	id					INT IDENTITY(01,1),
+	nome				VARCHAR(20) NOT NULL,
+	sobrenome			VARCHAR(20) NOT NULL,
+	senha				VARCHAR(50) NOT NULL,
+	celular				VARCHAR(14) NOT NULL,
+	data_nascimento		DATE NOT NULL,
+	cpf					VARCHAR(14) NOT NULL,
+	estado				VARCHAR(50) NOT NULL,
+	cidade				VARCHAR(100) NOT NULL,
+	bairro				VARCHAR(50) NOT NULL,
+	logradouro			VARCHAR(100) NOT NULL,
+	cep					VARCHAR(9) NOT NULL,
+	cargo				VARCHAR(20) NOT NULL
+);
