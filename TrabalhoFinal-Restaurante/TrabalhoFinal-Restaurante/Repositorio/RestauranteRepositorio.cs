0235a3f8f1655717c31bd20eef4b6e102cb81af1
Henrique Mateus Dalfovo Campos <hmdcampos@gmail.com>
2018-08-16 13:57:29 -0300

    BancoDados completo

---
+++ TrabalhoFinal-Restaurante/TrabalhoFinal-Restaurante/Repositorio/RestauranteRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TrabalhoFinal_Restaurante.Repositorio
+{
+    public class RestauranteRepositorio
+    {
+        public List<Restaurante> ObterTodos()
+        {
+            List<Restaurante> restaurante = new List<Restaurante>();
+            SqlCommand command = new BancoDados
+        }
+    }
+}
