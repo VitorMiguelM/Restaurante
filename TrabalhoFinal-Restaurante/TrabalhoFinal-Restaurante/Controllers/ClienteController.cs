51673ea8c2270b6534d1c8a7200a6445772e9483
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2018-08-16 14:31:27 -0300

    correção de erros

--- TrabalhoFinal-Restaurante/TrabalhoFinal-Restaurante/Controllers/ClienteController.cs
+++ TrabalhoFinal-Restaurante/TrabalhoFinal-Restaurante/Controllers/ClienteController.cs
@@ -11,7 +11,7 @@ namespace TrabalhoFinal_Restaurante.Controllers
         // GET: Cliente
         public ActionResult Index()
         {
-            ViewBag.TitluoPagina = "Restaurante ";
+            ViewBag.TitluoPagina = "Restaurante Luxo IV";
             return View();
         }
     }
