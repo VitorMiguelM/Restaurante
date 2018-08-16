679cb604b4bc75742231c8e3f60f941d5beee51a
ygorjuanwase <38700273+ygorjuanwase@users.noreply.github.com>
2018-08-16 15:14:25 -0300

    oi

---
+++ TrabalhoFinal-Restaurante/TrabalhoFinal-Restaurante/DataBase/BancoDados.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TrabalhoFinal_Restaurante.DataBase
+{
+    public class BancoDados
+    {
+        private static string connectionString;
+
+        static BancoDados()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        }
+
+        public SqlCommand
+    }
+}
---
+++ TrabalhoFinal-Restaurante/TrabalhoFinal-Restaurante/DataBase/Clientes.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE clientes(
+	id					INT IDENTITY(1,1),
+	nome_completo		VARCHAR(100) NOT NULL,
+	login				VARCHAR(12) NOT NULL,
+	senha				VARCHAR(12) NOT NULL,
+	celular				VARCHAR(14) NOT NULL,
+	data_nascimento		DATE NOT NULL,
+	cpf					VARCHAR(14) NOT NULL,
+	estado				VARCHAR(50) NOT NULL,
+	cidade				VARCHAR(100) NOT NULL,
+	bairo				VARCHAR(50) NOT NULL,
+	logradouro			VARCHAR(250) NOT NULL,
+	cep					VARCHAR(9) NOT NULL
+);
