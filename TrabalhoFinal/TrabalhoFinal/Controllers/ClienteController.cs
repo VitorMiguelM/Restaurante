9e1e888cda826f5464f2eeb3ddc11f2ef12498c2
ygorjuanwase <38700273+ygorjuanwase@users.noreply.github.com>
2018-08-17 15:03:06 -0300

    mais um comntroller criado

--- TrabalhoFinal/TrabalhoFinal/Controllers/ClienteController.cs
+++ TrabalhoFinal/TrabalhoFinal/Controllers/ClienteController.cs
@@ -3,16 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrabalhoFinal.Models;
+using TrabalhoFinal.Repositorios;
 
 namespace TrabalhoFinal.Controllers
 {
     public class ClienteController : Controller
     {
         // GET: Cliente
+        [HttpGet]
         public ActionResult Index()
         {
-            ViewBag.TitluoPagina = "Restaurante ";s
+            List<Clientes> clientes = new ClientesRepositorio().ObterTodos();
+            ViewBag.Cliente = clientes;
+            ViewBag.TituloPagina = "Clientes";
             return View();
+        }
+        [HttpGet]
+        public ActionResult CadastroCliente()
+        {
+            ViewBag.TituloPagina = "Restaurante-CadastroCliente";
+            ViewBag.Cliente = new Clientes();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            Clientes clientes = new ClientesRepositorio().
         }
     }
 }
