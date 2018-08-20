bb46337b7667da7ca9e108d4b7d1d55831b15295
ygorjuanwase <38700273+ygorjuanwase@users.noreply.github.com>
2018-08-20 15:15:49 -0300

    Funcionariocontroller criado terminado e mais nada

--- TrabalhoFinal/TrabalhoFinal/Controllers/ClienteController.cs
+++ TrabalhoFinal/TrabalhoFinal/Controllers/ClienteController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrabalhoFinal.Models;
+using TrabalhoFinal.Repositorios;
 
 namespace TrabalhoFinal.Controllers
 {
