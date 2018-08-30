0ac6c7f7079a690ea570c3f9503357c1fdd8cd02
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2018-08-30 17:23:13 -0300

    criado o controller "Lista" e seu html, modificações no cliente.cs (validação do numero de celular ). e criado também os comandos na navbar =D

--- TrabalhoFinal/TrabalhoFinal/Controllers/ClienteController.cs
+++ TrabalhoFinal/TrabalhoFinal/Controllers/ClienteController.cs
@@ -11,13 +11,21 @@ namespace TrabalhoFinal.Controllers
     public class ClienteController : Controller
     {
         // GET: Cliente
+        public ActionResult Lista()
+        {
+            List<Clientes> clientes = new ClientesRepositorio().ObterTodos();
+            ViewBag.Clientes = clientes;
+            return View();
+        }
+
+        // Pagina inicial do site
         public ActionResult Index()
         {
             ViewBag.TitluoPagina = "Restaurante Luxo IV ";
             return View();
         }
         [HttpGet]
-        public ActionResult CadastroCliente()
+        public ActionResult Cadastro()
         {
             ViewBag.TituloPagina = "Cadastro Cliente";
             ViewBag.Cliente = new Clientes();
