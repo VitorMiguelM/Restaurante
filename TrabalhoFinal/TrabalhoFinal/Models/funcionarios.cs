f0b43d1d8f1376f8a8e79101aae090fd152faccd
ygorjuanwase <38700273+ygorjuanwase@users.noreply.github.com>
2018-08-31 17:04:50 -0300

    trocando de computador pois este esta com problems

--- TrabalhoFinal/TrabalhoFinal/Models/funcionarios.cs
+++ TrabalhoFinal/TrabalhoFinal/Models/funcionarios.cs
@@ -13,29 +13,31 @@ namespace TrabalhoFinal.Models
         [MinLength(3, ErrorMessage = "Nome deve estar completo no minimo 3 carateres")]
         [MaxLength(100, ErrorMessage = "Nome deve conter no máximo 100 caracteres")]
         public string Nome { get; set; }
+
         [Required(ErrorMessage = "Sobrenome não pode ser vazio")]
         [MaxLength(100, ErrorMessage = "Sobrenome deve conter no máximo 100 caracteres")]
         public string Sobrenome { get; set; }
-          [Required(ErrorMessage = "Senha não pode ser vazio")]
+
+        [Required(ErrorMessage = "Senha não pode ser vazio")]
         public string Senha { get; set; }
-          [Required(ErrorMessage = "Celular não pode ser vazio")]
-        public  double Celular { get; set; }
-          [Required(ErrorMessage = "Data de nacimento não pode ser vazio")]
+        [Required(ErrorMessage = "Celular não pode ser vazio")]
+        public double Celular { get; set; }
+        [Required(ErrorMessage = "Data de nacimento não pode ser vazio")]
         public DateTime DataDeNascimento { get; set; }
-          [Required(ErrorMessage = "CPF não pode ser vazio")]
+        [Required(ErrorMessage = "CPF não pode ser vazio")]
         public string CPF { get; set; }
-          [Required(ErrorMessage = "Sobrenome não pode ser vazio")]
+        [Required(ErrorMessage = "Sobrenome não pode ser vazio")]
         public string Estado { get; set; }
-          [Required(ErrorMessage = "Cidade não pode ser vazio")]
+        [Required(ErrorMessage = "Cidade não pode ser vazio")]
 
         public string Cidade { get; set; }
-          [Required(ErrorMessage = "Bairro não pode ser vazio")]
+        [Required(ErrorMessage = "Bairro não pode ser vazio")]
         public string Bairro { get; set; }
-          [Required(ErrorMessage = "Logadouro não pode ser vazio")]
+        [Required(ErrorMessage = "Logadouro não pode ser vazio")]
         public string Logradouro { get; set; }
-          [Required(ErrorMessage = "CEP não pode ser vazio")]
+        [Required(ErrorMessage = "CEP não pode ser vazio")]
         public string CEP { get; set; }
-          [Required(ErrorMessage = "Cargo não pode ser vazio")]
+        [Required(ErrorMessage = "Cargo não pode ser vazio")]
         public string Cargo { get; set; }
 
 
