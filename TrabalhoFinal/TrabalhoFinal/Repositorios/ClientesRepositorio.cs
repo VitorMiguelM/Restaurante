280af0bb23b55032e2235e1aec93dd7ed08e8dc7
ygorjuanwase <38700273+ygorjuanwase@users.noreply.github.com>
2018-09-06 16:44:18 -0300

    corrigindo algumas coisas e melhoransdo o index

--- TrabalhoFinal/TrabalhoFinal/Repositorios/ClientesRepositorio.cs
+++ TrabalhoFinal/TrabalhoFinal/Repositorios/ClientesRepositorio.cs
@@ -24,7 +24,7 @@ namespace TrabalhoFinal.Repositorios
                 {
                     Id = Convert.ToInt32(linha[0].ToString()),
                     NomeCompleto = linha[1].ToString(),
-                    Login = linha[2].ToString(),
+                    Email = linha[2].ToString(),
                     Senha = linha[3].ToString(),
                     Celular = linha[4].ToString(),
                     DataNascimento = Convert.ToDateTime(linha[5].ToString()),
@@ -44,7 +44,7 @@ namespace TrabalhoFinal.Repositorios
             SqlCommand command = new BancoDados().ObterConexcao();
             command.CommandText = @"INSERT INTO clientes(nome_completo, login, senha, celular, data_nascimento, cpf, estado, cidade, logradoro, cep) OUTPUT INSERTED.ID VALUES (@NOME_COMPLETO, @LOGIN, @SENHA, @CELULAR, @DATA_NASCIMENTO, @CPF, @ESTADO, @CIDADE, @LOGRADOURO, @CEP)";
             command.Parameters.AddWithValue("@NOME_COMPLETO", clientes.NomeCompleto);
-            command.Parameters.AddWithValue("@LOGIN", clientes.Login);
+            command.Parameters.AddWithValue("@LOGIN", clientes.Email);
             command.Parameters.AddWithValue("@SENHA", clientes.Senha);
             command.Parameters.AddWithValue("@CELULAR", clientes.Celular);
             command.Parameters.AddWithValue("@DATA_NASCIMENTO", clientes.DataNascimento);
@@ -62,7 +62,7 @@ namespace TrabalhoFinal.Repositorios
             SqlCommand command = new BancoDados().ObterConexcao();
             command.CommandText = "UPDATE clientes SET nome_completo = @NOME_COMPLETO, login = @LOGIN, senha = @SENHA, data_nascimento = @DATA_NASCIMENTO, cpf = @CPF, estado = @ESTADO, cidade = @CIDADE, logradouro = @LOGRADOURO, cep = @CEP WHERE id = @ID";
             command.Parameters.AddWithValue("@NOME_COMPLETO", clientes.NomeCompleto);
-            command.Parameters.AddWithValue("@LOGIN", clientes.Login);
+            command.Parameters.AddWithValue("@LOGIN", clientes.Email);
             command.Parameters.AddWithValue("@SENHA", clientes.Senha);
             command.Parameters.AddWithValue("@CELULAR", clientes.Celular);
             command.Parameters.AddWithValue("@DATA_NASCIMENTO", clientes.DataNascimento);
@@ -96,7 +96,7 @@ namespace TrabalhoFinal.Repositorios
                 cliente = new Clientes();
                 cliente.Id = id;
                 cliente.NomeCompleto = table.Rows[0]["nome_completo"].ToString();
-                cliente.Login = table.Rows[0]["login"].ToString();
+                cliente.Email = table.Rows[0]["login"].ToString();
                 cliente.Senha = table.Rows[0]["senha"].ToString();
                 cliente.Celular = table.Rows[0]["celular"].ToString();
                 cliente.CPF = table.Rows[0]["cpf"].ToString();
