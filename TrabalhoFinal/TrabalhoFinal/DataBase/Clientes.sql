844f41c23a56c2440d573c78fc887e65692045c3
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2018-09-06 14:46:19 -0300

    correções no cliente.cs, no sql e no controller

--- TrabalhoFinal/TrabalhoFinal/DataBase/Clientes.sql
+++ TrabalhoFinal/TrabalhoFinal/DataBase/Clientes.sql
@@ -1,7 +1,8 @@
-﻿CREATE TABLE clientes(
+﻿DROP TABLE clientes;
+CREATE TABLE clientes(
 	id					INT IDENTITY(1,1),
 	nome_completo		VARCHAR(100) NOT NULL,
-	login				VARCHAR(12) NOT NULL,
+	email				VARCHAR(50) NOT NULL,
 	senha				VARCHAR(12) NOT NULL,
 	celular				VARCHAR(14) NOT NULL,
 	data_nascimento		DATE NOT NULL,
@@ -12,3 +13,5 @@
 	logradouro			VARCHAR(250) NOT NULL,
 	cep					VARCHAR(9) NOT NULL
 );
+
+INSERT INTO clientes (nome_completo, login, senha, celular, data_nascimento, cpf, estado, cidade, bairro, logradouro, cep) VALUES ('Austin Gillispie', 'austin12', 'austin12', '47 9563-5412', '1951-06-21', '505.310.755-67', 'Rio de Janeiro', 'Rio de Janeiro', 'Botafogo', 'Rua São Clemente', '22260-006');
--- TrabalhoFinal/TrabalhoFinal/Models/clientes.cs
+++ TrabalhoFinal/TrabalhoFinal/Models/clientes.cs
@@ -11,18 +11,13 @@ namespace TrabalhoFinal.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Nome não pode estar vazio.")]
-        [MinLength(3,ErrorMessage = "Nome deve conter no mínimo 10 caracteres. (nome e sobrenome)")]
+        [MinLength(3,ErrorMessage = "Nome deve conter no mínimo 10 caracteres. (nome e sobrenome).")]
         [MaxLength(50, ErrorMessage = "Nome deve conter no máximo 50 caracteres.")]
         public string NomeCompleto { get; set; }
 
-        [Required(ErrorMessage = "Login não pode estar vazio.")]
-        [MinLength(4, ErrorMessage = "Login deveconter no mínimo 4 caracteres.")]
-        [MaxLength(25, ErrorMessage = "Login deve conter no máximo 25 caracteres.")]
-        public string  Login { get; set; }
-
         [Required(ErrorMessage = "Senha não pode estar vazio.")]
         [MinLength(4, ErrorMessage = "Senha deve conter no mínimo 4 carateres.")]
-        [MaxLength(6, ErrorMessage = "Senha deve conter no máximo 6 caracteres")]
+        [MaxLength(6, ErrorMessage = "Senha deve conter no máximo 6 caracteres.")]
         public string Senha { get; set; }
 
         [Required(ErrorMessage = "Telefone Celular não pode estar vazio.")]
@@ -38,24 +33,25 @@ namespace TrabalhoFinal.Models
         [MaxLength(14, ErrorMessage = "Esse CPF é inválido!")]
         public string CPF { get; set; }
 
-        [Required(ErrorMessage = "Estado não pode estar vazio")]
+        [Required(ErrorMessage = "Estado não pode estar vazio.")]
         public string Estado { get; set; }
 
-        [Required(ErrorMessage = "Cidade não pode estar vazio")]
+        [Required(ErrorMessage = "Cidade não pode estar vazio.")]
         public string Cidade  { get; set; }
 
-        [Required(ErrorMessage = "Bairro não pode esar vazio")]
+        [Required(ErrorMessage = "Bairro não pode esar vazio.")]
         public string Bairro { get; set; }
 
-        [Required(ErrorMessage = "Logadouro não pode estar vazio")]
+        [Required(ErrorMessage = "Logadouro não pode estar vazio.")]
         public string Logradouro { get; set; }
 
-        [Required(ErrorMessage = "CEP não pode ser vazio")]
+        [Required(ErrorMessage = "CEP não pode ser vazio.")]
         [MinLength(8, ErrorMessage = "CEP deve conter no mínimo 8 caracteres")]
         [MaxLength(9, ErrorMessage = "CEP deve conter no máximo 9 caracteres")]
         public string CEP { get; set; }
 
-        [RegularExpression(@"^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,3})$", ErrorMessage = "Informe um email válido.")]
+        [RegularExpression(@"^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,3})$", ErrorMessage = "Informe um e-mail válido.")]
+        [Required(ErrorMessage = "E-mail não pode estar vazio.")]
         public string Email { get; set; }
     }
 }
