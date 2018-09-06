b6470abb59fd6eef5a8e5cbd42c7f185fe5c39d7
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2018-09-06 14:54:05 -0300

    alteração da "variavel" "login" > "email" no cliente repositório

--- TrabalhoFinal/TrabalhoFinal/Repositorios/ClientesRepositorio.cs
+++ TrabalhoFinal/TrabalhoFinal/Repositorios/ClientesRepositorio.cs
@@ -15,7 +15,7 @@ namespace TrabalhoFinal.Repositorios
         {
             List<Clientes> clientes = new List<Clientes>();
             SqlCommand command = new BancoDados().ObterConexcao();
-            command.CommandText = "SELECT id, nome_completo, login, senha, celular, data_nascimento, cpf, estado, cidade, logradouro, cep FROM clientes";
+            command.CommandText = "SELECT id, nome_completo, email, senha, celular, data_nascimento, cpf, estado, cidade, logradouro, cep FROM clientes";
             DataTable table = new DataTable();
             table.Load(command.ExecuteReader());
             foreach (DataRow linha in table.Rows)
@@ -24,7 +24,7 @@ namespace TrabalhoFinal.Repositorios
                 {
                     Id = Convert.ToInt32(linha[0].ToString()),
                     NomeCompleto = linha[1].ToString(),
-                    Login = linha[2].ToString(),
+                    Email = linha[2].ToString(),
                     Senha = linha[3].ToString(),
                     Celular = linha[4].ToString(),
                     DataNascimento = Convert.ToDateTime(linha[5].ToString()),
@@ -42,9 +42,9 @@ namespace TrabalhoFinal.Repositorios
         public int Cadastrar(Clientes clientes)
         {
             SqlCommand command = new BancoDados().ObterConexcao();
-            command.CommandText = @"INSERT INTO clientes(nome_completo, login, senha, celular, data_nascimento, cpf, estado, cidade, logradoro, cep) OUTPUT INSERTED.ID VALUES (@NOME_COMPLETO, @LOGIN, @SENHA, @CELULAR, @DATA_NASCIMENTO, @CPF, @ESTADO, @CIDADE, @LOGRADOURO, @CEP)";
+            command.CommandText = @"INSERT INTO clientes(nome_completo, email, senha, celular, data_nascimento, cpf, estado, cidade, logradoro, cep) OUTPUT INSERTED.ID VALUES (@NOME_COMPLETO, @EMAIL, @SENHA, @CELULAR, @DATA_NASCIMENTO, @CPF, @ESTADO, @CIDADE, @LOGRADOURO, @CEP)";
             command.Parameters.AddWithValue("@NOME_COMPLETO", clientes.NomeCompleto);
-            command.Parameters.AddWithValue("@LOGIN", clientes.Login);
+            command.Parameters.AddWithValue("@EMAIL", clientes.Email);
             command.Parameters.AddWithValue("@SENHA", clientes.Senha);
             command.Parameters.AddWithValue("@CELULAR", clientes.Celular);
             command.Parameters.AddWithValue("@DATA_NASCIMENTO", clientes.DataNascimento);
@@ -60,9 +60,9 @@ namespace TrabalhoFinal.Repositorios
         public bool Alterar(Clientes clientes)
         {
             SqlCommand command = new BancoDados().ObterConexcao();
-            command.CommandText = "UPDATE clientes SET nome_completo = @NOME_COMPLETO, login = @LOGIN, senha = @SENHA, data_nascimento = @DATA_NASCIMENTO, cpf = @CPF, estado = @ESTADO, cidade = @CIDADE, logradouro = @LOGRADOURO, cep = @CEP WHERE id = @ID";
+            command.CommandText = "UPDATE clientes SET nome_completo = @NOME_COMPLETO, email = @EMAIL, senha = @SENHA, data_nascimento = @DATA_NASCIMENTO, cpf = @CPF, estado = @ESTADO, cidade = @CIDADE, logradouro = @LOGRADOURO, cep = @CEP WHERE id = @ID";
             command.Parameters.AddWithValue("@NOME_COMPLETO", clientes.NomeCompleto);
-            command.Parameters.AddWithValue("@LOGIN", clientes.Login);
+            command.Parameters.AddWithValue("@EMAIL", clientes.Email);
             command.Parameters.AddWithValue("@SENHA", clientes.Senha);
             command.Parameters.AddWithValue("@CELULAR", clientes.Celular);
             command.Parameters.AddWithValue("@DATA_NASCIMENTO", clientes.DataNascimento);
@@ -87,7 +87,7 @@ namespace TrabalhoFinal.Repositorios
         {
             Clientes cliente = null;
             SqlCommand command = new BancoDados().ObterConexcao();
-            command.CommandText = "SELECT nome_completo, login, senha, celular, data_nascimento, cpf, estado, cidade, logradouro, cep FROM clientes WHERE id = @ID";
+            command.CommandText = "SELECT nome_completo, email, senha, celular, data_nascimento, cpf, estado, cidade, logradouro, cep FROM clientes WHERE id = @ID";
             command.Parameters.AddWithValue("@ID", id);
             DataTable table = new DataTable();
             table.Load(command.ExecuteReader());
@@ -96,7 +96,7 @@ namespace TrabalhoFinal.Repositorios
                 cliente = new Clientes();
                 cliente.Id = id;
                 cliente.NomeCompleto = table.Rows[0]["nome_completo"].ToString();
-                cliente.Login = table.Rows[0]["login"].ToString();
+                cliente.Email = table.Rows[0]["email"].ToString();
                 cliente.Senha = table.Rows[0]["senha"].ToString();
                 cliente.Celular = table.Rows[0]["celular"].ToString();
                 cliente.CPF = table.Rows[0]["cpf"].ToString();
