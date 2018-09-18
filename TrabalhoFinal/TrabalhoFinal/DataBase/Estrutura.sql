05ae70d4447293566783b067931046f329c2450e
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2018-09-18 16:49:50 -0300

    criado "login" novamente, na sql, cliente.cs, repositorio,etc

--- TrabalhoFinal/TrabalhoFinal/DataBase/Estrutura.sql
+++ TrabalhoFinal/TrabalhoFinal/DataBase/Estrutura.sql
@@ -2,6 +2,7 @@
 	id					INT IDENTITY(1,1),
 	nome_completo		VARCHAR(100) NOT NULL,
 	[email]				VARCHAR(50) NOT NULL,
+	login				VARCHAR(50) NOT NULL,
 	senha				VARCHAR(12) NOT NULL,
 	celular				VARCHAR(14) NOT NULL,
 	data_nascimento		DATE NOT NULL,
