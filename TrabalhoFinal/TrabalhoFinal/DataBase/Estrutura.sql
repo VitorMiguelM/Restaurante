d2aefe24f764905397e1e8c61c2ab63ce434d4b1
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2018-09-20 16:52:45 -0300

    att

--- TrabalhoFinal/TrabalhoFinal/DataBase/Estrutura.sql
+++ TrabalhoFinal/TrabalhoFinal/DataBase/Estrutura.sql
@@ -1,4 +1,11 @@
-﻿CREATE TABLE clientes(
+﻿DROP TABLE funcionarios;
+DROP TABLE ingredientes_pratos;
+DROP TABLE pratos;
+DROP TABLE ingredientes;
+DROP TABLE reservas;
+DROP TABLE clientes;
+
+CREATE TABLE clientes(
 	id					INT IDENTITY(1,1),
 	nome_completo		VARCHAR(100) NOT NULL,
 	[email]				VARCHAR(50) NOT NULL,
@@ -64,12 +71,9 @@ CREATE TABLE funcionarios(
 	cep					VARCHAR(9) NOT NULL,
 	cargo				VARCHAR(20) NOT NULL
 );
-DROP TABLE funcionarios;
-DROP TABLE ingredientes_pratos;
-DROP TABLE pratos;
-DROP TABLE ingredientes;
-DROP TABLE reservas;
-DROP TABLE clientes;
+
+
+
 
 
 INSERT INTO reservas(login, senha, nome, celular, cpf, pagamento) VALUES('hmdcampos@gmail.com','991310455','Henrique Mateus Dalfovo Campos','(47) 99131-0455','052.186.589-10','Dinheiro');
